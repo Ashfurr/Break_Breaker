cccb7f7b166b6eb76867f6518052975c0d7949d0
x1301 <92451531+x1301@users.noreply.github.com>
2022-02-05 19:45:16 +0100

    Joueur

--- src/Joueur.js
+++ src/Joueur.js
@@ -1,5 +1,8 @@
 class Joueur {
     constructor(){
+        this.Tablo= Tableau1
 
+        this.player=this.Tablo.physics.add.sprite(500,y,'pad').setOrigin(0,0)
+        this.player.body.setSize(40,100)
     }
 }
--- src/Tableau1.js
+++ src/Tableau1.js
@@ -4,6 +4,7 @@ class Tableau1 extends Phaser.Scene {
 
 
     preload(){
+        this.load.image('pad','assets/carre.png')
     }
     create(){
     }
