a6eebae7e18cb3329e950eafb3db42f00c293b2b
Ashfurr <39094834+Ashfurr@users.noreply.github.com>
2022-02-06 19:47:16 +0100

    Update Ball.js
    Reset ball

--- src/Ball.js
+++ src/Ball.js
@@ -3,4 +3,10 @@ class Ball {
     this.scene=Tableau1
         this.ballC=this.scene.physics.add.sprite(gameConfig.width/2,550,'pad').setDisplaySize(20, 20).setVelocityY(500).setBounce(1,1);
     }
+Reset(){
+  this.ballC.x=gameConfig.width/2
+  this.ballC.y=550
+  this.ballC.setVelocityX(0)
+  this.ballC.setVelocityY(500)
+  }
 }
